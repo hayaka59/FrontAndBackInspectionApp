828b28511e05218eed7e418971eb6b7514de9c95
Hayakawa Shinji <hayaka59@mbox.kyoto-inet.or.jp>
2025-11-20 15:24:04 +0900

    update:バージョン表示ラベルの大きさと表示位置の変更

--- FrontAndBackInspectionApp/表裏検査装置画面/MainMenuForm.Designer.cs
+++ FrontAndBackInspectionApp/表裏検査装置画面/MainMenuForm.Designer.cs
@@ -60,9 +60,9 @@
             // 
             this.LblVersion.Font = new System.Drawing.Font("メイリオ", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.LblVersion.ForeColor = System.Drawing.Color.Blue;
-            this.LblVersion.Location = new System.Drawing.Point(1761, 1008);
+            this.LblVersion.Location = new System.Drawing.Point(1700, 1000);
             this.LblVersion.Name = "LblVersion";
-            this.LblVersion.Size = new System.Drawing.Size(130, 25);
+            this.LblVersion.Size = new System.Drawing.Size(170, 30);
             this.LblVersion.TabIndex = 315;
             this.LblVersion.Text = "LblVersion";
             this.LblVersion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
