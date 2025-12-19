6f5f3cf5bed499c60b58d59221f1d4c3c11317bb
Hayakawa Shinji <hayaka59@mbox.kyoto-inet.or.jp>
2025-12-19 11:31:57 +0900

    add:終了画面のWindowsシャットダウン処理の追加

--- FrontAndBackInspectionApp/表裏検査装置画面/EndForm.cs
+++ FrontAndBackInspectionApp/表裏検査装置画面/EndForm.cs
@@ -64,12 +64,52 @@ namespace FrontAndBackInspectionApp.表裏検査装置画面
             try
             {
                 Log.OutPutLogFile(TraceEventType.Information, "終了画面：「終了実行」ボタンクリック");
+
+                if (ChkShutDown.Checked)
+                { 
+                    TryShutdownWindows();
+                }                
                 // アプリケーションを終了します
                 Application.Exit();
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "【BtnEnd_Click】", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void TryShutdownWindows()
+        {
+            var psi = new ProcessStartInfo
+            {
+                FileName = "shutdown",
+                Arguments = "/s /t 0", // /s: シャットダウン, /t 0: 即時
+                UseShellExecute = true,
+                Verb = "runas",        // 管理者として実行（UAC昇格）
+                CreateNoWindow = true
+            };
+
+            //var psi = new ProcessStartInfo
+            //{
+            //    FileName = "notepad",
+            //    //Arguments = "/s /t 0", // /s: シャットダウン, /t 0: 即時
+            //    UseShellExecute = true,
+            //    //Verb = "runas",        // 管理者として実行（UAC昇格）
+            //    CreateNoWindow = false
+            //};
+
+            try
+            {
+                Process.Start(psi);
+            }
+            catch (Exception ex)
+            {
+                // 権限拒否などの例外ハンドリング
+                MessageBox.Show("シャットダウンの開始に失敗しました。\n" + ex.Message,
+                                "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
     }
