56ed6e1d0076a2fd2efb47759d7f1f4ace495b8c
Hayakawa Shinji <hayaka59@mbox.kyoto-inet.or.jp>
2026-01-15 17:39:50 +0900

    chore:シミュレーターのバージョン情報を「Ver.20.26.01.15」とする

--- FrontAndBackSimulatorApp/PubConstClass.cs
+++ FrontAndBackSimulatorApp/PubConstClass.cs
@@ -20,7 +20,7 @@ namespace FrontAndBackSimulatorApp
         public const string CMD_RECIEVE_d = "Zd";   // 
         public const string CMD_RECIEVE_e = "Ze";   // 
 
-        public const string DEF_VERSION = "Ver.20.26.01.10";                        // バージョン情報（メジャー.マイナー.ビルド.リビジョン） 
+        public const string DEF_VERSION = "Ver.20.26.01.15";                        // バージョン情報（メジャー.マイナー.ビルド.リビジョン） 
         public static object objSyncHist;
 
         public const string DEF_FILENAME = "FrontAndBackSimulatorApp.def";          // DEFファイル名称
